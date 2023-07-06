31c496cc2a8936d7a9ef13ff76613c1d26f4bd43
FPolicastro <137439283+FPolicastro@users.noreply.github.com>
2023-07-06 09:38:47 -0400

    Spring Initializr Generation
    Generated an empty project with no dependencies.

---
+++ policastro_project/src/main/java/com/skillstorm/policastro_project/PolicastroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.policastro_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicastroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicastroProjectApplication.class, args);
+	}
+
+}
